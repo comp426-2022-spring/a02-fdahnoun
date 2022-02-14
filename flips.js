b25c22b0aedb99679ee37babb79cd409b62b9dd4
fdahnoun <fdahnoun@email.unc.edu>
2022-02-14 00:48:36 -0500

    please

--- flips.js
+++ flips.js
@@ -1,1 +1,10 @@
-//please
+import { coinFlips, countFlips } from './modules/coin.mjs';
+import minimist from 'minimist';
+
+const commInput = minimist(process.argv.slice(2));
+
+const input = commInput.number;
+
+var totalResults = coinFlips(input);
+console.log(totalResults);
+console.log(countFlips(totalResults));
