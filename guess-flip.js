cd058a486281d5df2acf83c1e9040a19f11eed7b
fdahnoun <fdahnoun@email.unc.edu>
2022-02-15 18:32:37 -0500

    fixed error in guess-flip

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ import minimist from 'minimist';
 const commInput = minimist(process.argv.slice(2));
 if (commInput.call != "heads" && commInput.call != "tails"){
     console.log("Error: no input.\n")
-    console.log("Usage: node guess-flip=[heads|tails]")
+    console.log("Usage: node guess-flip --call=[heads|tails]")
 } else{
     console.log(flipACoin(commInput.call))
 }
