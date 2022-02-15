efaa94b49be93e53705d18433148b6dd619cd84a
fdahnoun <fdahnoun@email.unc.edu>
2022-02-15 18:03:08 -0500

    fixed guess-flip

--- guess-flip.js
+++ guess-flip.js
@@ -3,8 +3,8 @@ import minimist from 'minimist';
 
 const commInput = minimist(process.argv.slice(2));
 if (commInput.call != "heads" && commInput.call != "tails"){
-    console.log("Error: no input.")
-    console.log("Usage: node guess-flip=[heads|tails]")
+    console.log("Error: no input.\nUsage: node guess-flip=[heads|tails]")
+    //console.log("Usage: node guess-flip=[heads|tails]")
 } else{
     console.log(flipACoin(commInput.call))
 }
